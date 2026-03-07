f4cdc23d4afad1290e4c571d576d62d41e1a7abd
Truong Nguyen <165233624+Jamejsskjdad@users.noreply.github.com>
2026-03-08 02:54:32 +0700

    Update name làng chè

--- src/controllers/page.controller.js
+++ src/controllers/page.controller.js
@@ -1,6 +1,6 @@
 exports.renderHome = (req, res) => {
     res.render("index", {
-      siteTitle: "Làng Chè Việt Nam",
+      siteTitle: "Làng Chè Thái Nguyên",
       heroSubtitle: "Khám phá văn hóa trà Việt"
     });
   };
